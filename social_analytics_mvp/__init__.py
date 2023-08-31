58c073d4cda701fa75498b346c79b656b2b29810
Olivier Dupuis <odupuis@lantrns.co>
2023-08-31 10:44:03 -0400

    Renaming apps

--- social_analytics_mvp/__init__.py
+++ social_analytics_mvp/__init__.py
@@ -5,21 +5,21 @@ from dagster import (
 )
 from dagster_dbt import dbt_cli_resource, load_assets_from_dbt_project
 
-dp_source_prep_project_dir = file_relative_path(__file__, "./dp_source_prep/")
-dp_source_prep_profile_dir = file_relative_path(__file__, "./dp_source_prep/configs/")
+dp_source_data_project_dir = file_relative_path(__file__, "./dp_source_data/")
+dp_source_data_profile_dir = file_relative_path(__file__, "./dp_source_data/configs/")
 
 my_assets = with_resources(
     load_assets_from_dbt_project(
-        project_dir = dp_source_prep_project_dir, 
-        profiles_dir = dp_source_prep_profile_dir, 
-        key_prefix = ["dp_source_prep"],
+        project_dir = dp_source_data_project_dir, 
+        profiles_dir = dp_source_data_profile_dir, 
+        key_prefix = ["dp_source_data"],
         use_build_command = True
     ),
     resource_defs = {
         "dbt": dbt_cli_resource.configured(
             {
-                "project_dir": dp_source_prep_project_dir,
-                "profiles_dir": dp_source_prep_profile_dir,
+                "project_dir": dp_source_data_project_dir,
+                "profiles_dir": dp_source_data_profile_dir,
             },
         ),
     },
