f989af164634f82754ff957743fef198195b117b
Olivier Dupuis <odupuis@lantrns.co>
2023-11-01 15:53:19 -0400

    Stripping data product and refactoring to introduce Snowflake

--- freedom_convoy/stage_articles/models/staging/stg__enhanced_articles__articles.sql
+++
@@ -1,9 +0,0 @@
-with base as (
-
-    select *
-        
-    from {{ source('enhanced_articles', 'articles') }}
-
-)
-
-select * from base
--- freedom_convoy/utils/resources/my_resources.py
+++ freedom_convoy/utils/resources/my_resources.py
@@ -1,5 +1,13 @@
+from dagster_snowflake import snowflake_resource
 from freedom_convoy.utils.resources import web_scraper_resource
 
 
+# Configuration files
+snowflake_configs = config_from_pkg_resources(
+    pkg_resource_defs=[
+        ('discursus_data_platform.utils.configs', 'snowflake_configs.yaml')
+    ],
+)
 # Initiate resources
+my_snowflake_resource = snowflake_resource.configured(snowflake_configs)
 my_web_scraper_resource = web_scraper_resource.initiate_web_scraper_resource.configured(None)
