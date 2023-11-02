7a42dca51f9c21770b25227d9ae8731c50e1b956
Olivier Dupuis <odupuis@lantrns.co>
2023-11-02 14:06:16 -0400

    Sourcing signals from Snowflake

--- freedom_convoy/data_products/social_signals/gdelt.py
+++ freedom_convoy/data_products/social_signals/gdelt.py
@@ -1,17 +1,12 @@
 from dagster import (
     asset,
-    AssetIn,
-    AssetKey,
-    AutoMaterializePolicy,
-    DynamicPartitionsDefinition,
-    FreshnessPolicy,
     Output
 )
-from dagster_aws.s3 import s3_pickle_io_manager, s3_resource
 
 import pandas as pd
 
 from freedom_convoy.utils.resources import my_resources
+
 
 @asset(
     description = "Freedom convoy articles sample",
@@ -20,13 +15,39 @@ from freedom_convoy.utils.resources import my_resources
     resource_defs = {
         'snowflake_resource': my_resources.my_snowflake_resource,
     },
+    compute_kind = "snowflake",
+)
+def media_sources(context):
+    # Fetch media sources
+    query = f"""
+        select * from social_signals.gdelt.media_sources
+    """
+    media_sources_df = context.resources.snowflake_resource.execute_query(query, fetch_results=True, use_pandas_result=True)
+    
+    # Return asset
+    return Output(
+        value = media_sources_df,
+        metadata = {
+            "rows": media_sources_df.index.size,
+        },
+    )
+
+
+@asset(
+    description = "Freedom convoy articles sample",
+    key_prefix = ["social_signals"],
+    group_name = "social_signals",
+    resource_defs = {
+        'snowflake_resource': my_resources.my_snowflake_resource,
+    },
+    compute_kind = "snowflake",
 )
 def gdelt_articles(context):
     # Fetch articles
     query = f"""
-        select * from select * from social_signals.gdelt.gdelt_gkg_sample
+        select * from social_signals.gdelt.gdelt_gkg_sample
     """
-    gdelt_articles_df = context.resources.snowflake_resource.execute_query(query)
+    gdelt_articles_df = context.resources.snowflake_resource.execute_query(query, fetch_results=True, use_pandas_result=True)
     
     # Return asset
     return Output(
